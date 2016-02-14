b17244f98409e27f170293b2ca4772af13e0e8c8
Jonathan <jgibler@gmail.com>
2016-02-14 23:14:13 +0000

    public img

--- app.js
+++ app.js
@@ -8,16 +8,14 @@ mongoose.connect('mongodb://dbuser:dbpassword@ds047114.mongolab.com:47114/experi
 
 //routes
 app.all('/', function (req, res) {
-  res.send('Hello Cats!');
+  res.send('<img src="img/cats.jpg" /><br /><a href="/cats">Cats</a>');
 });
-
 app.get('/cats', function (req, res) {
   res.sendFile(__dirname + '/views/cats.html');
 });
-
 app.use('/api/cats', require('./api/cats.js'));
-
 app.use('/controllers', express.static(__dirname + '/controllers'));
+app.use('/', express.static(__dirname + '/public'));
 
 //start server
 app.listen(80, function () {
